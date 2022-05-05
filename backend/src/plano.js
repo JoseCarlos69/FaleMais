e8ce6d12d247a44ff4a733f216b8ab84be526df3
JoseCarlos69 <jjc.silva10@gmail.com>
2022-05-05 11:13:36 -0300

    Adicionando frontend

--- backend/src/plano.js
+++ backend/src/plano.js
@@ -1,9 +1,9 @@
 class Plano {
-    constructor(nome, franquia, acrescimo) {
-        this.nome = nome;
-        this.franquia = franquia;
-        this.acrescimo = acrescimo;
-    }
+  constructor(nome, franquia, acrescimo) {
+      this.nome = nome;
+      this.franquia = franquia;
+      this.acrescimo = acrescimo;
   }
-  
-  module.exports = Plano;
+}
+
+module.exports = Plano;
